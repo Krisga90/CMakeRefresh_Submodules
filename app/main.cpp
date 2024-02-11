395d7c7db8c0fe288b0bcc33e674f4fe67aa1f39
krisga90 <krzysztof.meyer@gmail.com>
2024-02-11 22:34:10 +0100

    submodule

--- app/main.cpp
+++ app/main.cpp
@@ -3,12 +3,21 @@
 #include "my_lib.hpp"
 #include "config.hpp"
 
+#include <nlohmann/json.hpp>
+
 int main(int argc, char** argv)
 {
 
     std::cout << project_name << "\n";
     std::cout << project_version << "\n";
-    print_hello_world();
+    // print_hello_world();
+
+    std::cout << "LIb version nlohmann: "
+        << NLOHMANN_JSON_VERSION_MAJOR << "."
+        << NLOHMANN_JSON_VERSION_MINOR << "."
+        << NLOHMANN_JSON_VERSION_PATCH << "\n";
+
+    cos();
 
     return 0;
 }
